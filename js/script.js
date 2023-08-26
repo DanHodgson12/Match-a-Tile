58930eaecb3ba67d22c6d90536402a897789941b
DanHodgson12 <>
2023-08-26 17:38:15 +0000

    Added function to change the display of the Mode when one is selected from the dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
+// Side-bar info toggle divs
 $('#about-btn').click(function() {
   $('#about-text').slideToggle('slow');
 });
 
 $('#instructions-btn').click(function () {
   $('#instructions-text').slideToggle('slow');
+});
+
+// Mode-selection
+function modeDisplay(mode) {
+
+  if (mode === 'Easy') {
+    $('#mode-display').html('EASY');
+  } else if (mode === 'Normal') {
+    $('#mode-display').html('NORMAL');
+  } else if (mode === 'Hard') {
+    $('#mode-display').html('HARD');
+  }
+
+}
+
+$('.mode-item').click(function() {
+  let modeSelection = $(this).data('value');
+  modeDisplay(modeSelection);
 });
